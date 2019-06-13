e3b7568edf3e857981da5f5c1dc9460b6e2bec4d
phanikumarsharma <31402166+phanikumarsharma@users.noreply.github.com>
2019-06-13 17:57:48 +0530

    Update New-monitoruxSetup.ps1

--- New-monitoruxSetup.ps1
+++ New-monitoruxSetup.ps1
@@ -1,4 +1,3 @@
-   
 $subscriptionid = Get-AutomationVariable -Name 'subscriptionid'
 $ResourceGroupName = Get-AutomationVariable -Name 'ResourceGroupName'
 $fileURI = Get-AutomationVariable -Name 'fileURI'
@@ -7,51 +6,41 @@ $Password = Get-AutomationVariable -Name 'Password'
 $automationAccountName = Get-AutomationVariable -Name 'accountName'
 $WebApp = Get-AutomationVariable -Name 'webApp'
 $WorkspaceID = Get-AutomationVariable -Name 'WorkspaceID'
-
 Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process -Force -Confirm:$false
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force -Confirm:$false
 Get-ExecutionPolicy -List
-
 Invoke-WebRequest -Uri $fileURI -OutFile "C:\wvd-monitoring-ux.zip"
 New-Item -Path "C:\wvd-monitoring-ux" -ItemType directory -Force -ErrorAction SilentlyContinue
 Expand-Archive "C:\wvd-monitoring-ux.zip" -DestinationPath "C:\wvd-monitoring-ux" -ErrorAction SilentlyContinue
-
 $modules="https://raw.githubusercontent.com/Azure/RDS-Templates/wvd-mgmt-ux/wvd-templates/wvd-management-ux/deploy/scripts/msft-wvd-saas-offering.zip"
 Invoke-WebRequest -Uri $modules -OutFile "C:\msft-rdmi-saas-offering.zip"
 New-Item -Path "C:\msft-rdmi-saas-offering" -ItemType directory -Force -ErrorAction SilentlyContinue
 Expand-Archive "C:\msft-rdmi-saas-offering.zip" -DestinationPath "C:\msft-rdmi-saas-offering" -ErrorAction SilentlyContinue
 $AzureModulesPath = Get-ChildItem -Path "C:\msft-rdmi-saas-offering\msft-wvd-saas-offering"| Where-Object {$_.FullName -match 'AzureModules.zip'}
 Expand-Archive $AzureModulesPath.fullname -DestinationPath 'C:\Modules\Global' -ErrorAction SilentlyContinue
-
 Import-Module AzureRM.Resources
 Import-Module AzureRM.Profile
 Import-Module AzureRM.Websites
 Import-Module Azure
 Import-Module AzureRM.Automation
 Import-Module AzureAD
-
     
 #The name of the Automation Credential Asset this runbook will use to authenticate to Azure.
 $CredentialAssetName = 'DefaultAzureCredential'
-
 #Get the credential with the above name from the Automation Asset store
 $Cred = Get-AutomationPSCredential -Name $CredentialAssetName
 Add-AzureRmAccount -Environment 'AzureCloud' -Credential $Cred
 Select-AzureRmSubscription -SubscriptionId $subscriptionid
-
 Write-Output "Getting the Publishing profile information from Web-App"
 $WebAppXML = (Get-AzureRmWebAppPublishingProfile -Name $WebApp `
 -ResourceGroupName $ResourceGroupName  `
 -OutputFile null)
 $WebAppXML = [xml]$WebAppXML
-
 # Extract connection information from publishing profile
-
 Write-Output "Gathering the username, password and publishurl from the Web-App Publishing Profile"
 $WebAppUserName = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userName").value
 $WebAppPassword = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@userPWD").value
 $WebAppURL = $WebAppXML.SelectNodes("//publishProfile[@publishMethod=`"MSDeploy`"]/@publishUrl").value
-
 # Publish Web-App Package files recursively
 Write-Output "Uploading zip file to web-App"
 #Get-ChildItem $appdirectory -recurse | Compress-Archive -update -DestinationPath 'c:\WebApp-Monitor-UX.zip' -Verbose 
@@ -62,23 +51,19 @@ $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 $userAgent = "powershell/1.0"
 Invoke-RestMethod -Uri $apiURL -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
 # Get Url of Web-App
-
 $GetWebApp = Get-AzureRmWebApp -Name $WebApp -ResourceGroupName $ResourceGroupName
 $WebURL = $GetWebApp.DefaultHostName         
 $redirectURL="https://"+"$WebURL"
-
 $Psswd = $Password | ConvertTo-SecureString -asPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential($Username,$Psswd)
 #Install-Module -Name AzureAD
 Connect-AzureAD -AzureEnvironmentName AzureCloud -Credential $Credential
-
 $modules="https://raw.githubusercontent.com/phanikumarsharma/mytestrepo/master/testappreg1106.ps1"
 Invoke-WebRequest -Uri $modules -OutFile "C:\testappreg1106.ps1"
 Set-Location "C:\"
 .\testappreg1106.ps1 -subscriptionid $subscriptionid -Username $Username -Password $Password -WebApp $WebApp -redirectURL $redirectURL
 $appreg=Get-AzureADApplication -SearchString $WebApp
 $ClientId=$appreg.AppId
-
 # Adding App Settings to WebApp
 Write-Output "Adding App settings to Web-App"
 $WebAppSettings = @{
@@ -86,21 +71,16 @@ $WebAppSettings = @{
     "AzureAd:WorkspaceID" = "$WorkspaceID"
 }
 Set-AzureRmWebApp -AppSettings $WebAppSettings -Name $WebApp -ResourceGroupName $ResourceGroupName
-
 $newReplyUrl = "$redirectURL/security/signin-callback"
 # Get Azure AD App
 $app = Get-AzureADApplication -Filter "AppId eq '$($ClientId)'"
-
 $replyUrls = $app.ReplyUrls
-
-
 # Add Reply URL if not already in the list 
-
 if ($replyUrls -NotContains $newReplyUrl) {
     $replyUrls.Add($newReplyUrl)
     Set-AzureADApplication -ObjectId $app.ObjectId -ReplyUrls $replyUrls -Verbose -ErrorAction Stop
 }
-
+    
 #set Log analytics api permission to Client App Registration
 $AzureLogAnalyticsApiPrincipal = Get-AzureADServicePrincipal -SearchString "Log Analytics API"
 $AzureLogAnalyticsApiAccess = New-Object -TypeName "Microsoft.Open.AzureAD.Model.RequiredResourceAccess"
@@ -131,24 +111,19 @@ Import-Module AzureRM.Automation
 `$login=Login-AzureRmAccount -Credential `$Azurecred -SubscriptionId `$SubscriptionId
 Remove-AzureRmAutomationAccount -Name `$automationAccountName -ResourceGroupName `$ResourceGroupName -Force 
 "@| Out-File -FilePath RemoveAccount:\RemoveAccount.ps1 -Force
-
     $runbookName='removewvdsaasacctbook'
     #Create a Run Book
     New-AzureRmAutomationRunbook -Name $runbookName -Type PowerShell -ResourceGroupName $ResourceGroupName -AutomationAccountName $automationAccountName
-
     #Import modules to Automation Account
     $modules="AzureRM.profile,Azurerm.compute,azurerm.resources"
     $modulenames=$modules.Split(",")
     foreach($modulename in $modulenames){
     Set-AzureRmAutomationModule -Name $modulename -AutomationAccountName $automationAccountName -ResourceGroupName $ResourcegroupName
     }
-
     #Importe powershell file to Runbooks
     Import-AzureRmAutomationRunbook -Path "C:\RemoveAccount.ps1" -Name $runbookName -Type PowerShell -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName -Force
-
     #Publishing Runbook
     Publish-AzureRmAutomationRunbook -Name $runbookName -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName
-
     #Providing parameter values to powershell script file
     $params=@{"UserName"=$UserName;"Password"=$Password;"ResourcegroupName"=$ResourcegroupName;"SubscriptionId"=$subscriptioni;"automationAccountName"=$automationAccountName}
     Start-AzureRmAutomationRunbook -Name $runbookName -ResourceGroupName $ResourcegroupName -AutomationAccountName $automationAccountName -Parameters $params | Out-Null
